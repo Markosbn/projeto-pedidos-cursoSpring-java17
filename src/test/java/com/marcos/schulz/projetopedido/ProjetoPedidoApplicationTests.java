6ae7a175e013535e75c1f3af32b6b9dddb0e7b14
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-14 11:56:27 -0300

    projeto criado

---
+++ src/test/java/com/marcos/schulz/projetopedido/ProjetoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.schulz.projetopedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
