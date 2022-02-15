f26f19b8831df0bc79728a2e3ba66ab00a59e996
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-15 14:10:35 -0300

    Adicionado as funções para subtotal dos items e total do pedido

--- src/main/java/com/marcos/schulz/projetopedido/domain/models/Order.java
+++ src/main/java/com/marcos/schulz/projetopedido/domain/models/Order.java
@@ -95,6 +95,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+        Double total= 0.0;
+        for (OrderItem x : items){
+            total += x.getSubTotal();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
